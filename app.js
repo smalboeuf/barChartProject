2409c4521983b14dd13a8b457f6c5f482dd5e469
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-01-14 19:45:08 +0000

    Added dynamic bar chart generation based on how many numbers are in data array

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let titleFontSize = 2;
 let titleFontColour = "black";
 
 //Data
-let barData = [1, 2, 3, 4];
+let barData = [1, 2, 3, 4, 5];
 let barOptions = {
   width: 2,
   height: 3
@@ -24,31 +24,50 @@ let numOfBars = barData.length;
 
 
 
+console.log(setBarLengths(barData, ".chart"));
 
 
-let drawBarChart = function(data, options, element){
+
+function drawBarChart(data, options, element){
 //data is an array
 //options is an object (bar colour, label colour, bar spacing, bar chart axes )
 //element will be a DOM element name
-  barData = data;
-  DOMElement = element;
+
+
+  DOMElement = "chart";
 
   //Change the bar height
-  setBarLengths(barData);
+  barData = data;
+  setBarLengths(barData, DOMElement);
 
   //Change the bar settings
   setBarOptions(options);
 
 };
 
-let setBarOptions = function(options){
+function setBarLengths(options){
   //Settings the barOptions based on the object
   barOptions.height = options.height;
   barOptions.width = options.width;
 }
 
-let setBarLengths = function(barData){
+function setBarLengths(barData, DOMElement){
    //Setting the length of the bar based on the data
+   let chartDiv = document.querySelector(DOMElement);
+   console.log(chartDiv);
+  for(let i = 0; i < barData.length; i++){
+    let barTemplate = document.createElement("div");
+    barTemplate.classList.add("barDiv");
+    barTemplate.innerHTML += "<h4>Title "+(i+1)+"</h4>";
+    barTemplate.innerHTML += "<div class = bar><div class = barLevel></div></div>"
+
+    chartDiv.appendChild(barTemplate);
+
+  }
+
+  console.log(chartDiv);
+
+
    let barLengths = document.querySelectorAll('.barLevel');
    for(let i = 0; i < barData.length; i++){
      barLengths[i].style.width = barData[i] + "rem";
