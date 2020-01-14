edec4ad7ebc039f8c2014e1f310fece1c4730244
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-01-14 17:59:53 +0000

    second commit. Basic layout and styling of chart

---
+++ app.js
@@ -0,0 +1,30 @@
+//Initializing variables
+
+
+let barWidth = 20;  //depends on the amount of values passed
+let barHeight = 20; //Based off of data values passed
+
+let barColour = "skyblue";
+let labelColour = "orange";
+let barSpacing = 1;
+let barChartAxis = "x"; //horizontal or vertical
+
+//Bar chart title cuztomizable properties
+let titleFontSize = 2;
+let titleFontColour = "black";
+
+
+
+
+let drawBarChart = function(data, options, element){
+//data is an array
+//options is an object (bar colour, label colour, bar spacing, bar chart axes )
+//element will be a DOM element name
+
+
+};
+
+
+
+
+
